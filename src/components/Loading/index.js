aec63d773d5f882e01a66e8d0e6b5945dd48ec41
Cruason <94239919+Cruason@users.noreply.github.com>
2023-12-17 23:08:07 +0300

    Update index.js

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -2,7 +2,7 @@ import styled, { keyframes } from "styled-components";
 
 const spinAnimation = keyframes`
   0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
+  100% { transform: rotate(360deg);  }
 `;
 
 export const SpinnerContainer = styled.div`
