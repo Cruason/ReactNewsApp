9035f30eb649626a69364c70ae236e5093d10570
Cruason <94239919+Cruason@users.noreply.github.com>
2023-12-17 23:08:57 +0300

    Update index.js

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -11,7 +11,7 @@ export const Header = styled.h1`
 `;
 
 export const Container = styled.div`
-  width: 93%;
+  width: 93% ;
   padding-right: (1.5rem, 0.75rem);
   padding-left: (1.5rem, 0.75rem);
   margin-right: auto;
